1b2bf8a48d66eb8a16f7463edbe3ae9d908d7db4
Rafal <rzywczak99@gmail.com>
2026-06-02 12:54:58 +0200

    fix(ui): improve component structure and formatting for better readability

--- 02-starting-project/src/components/MainContent.jsx
+++ 02-starting-project/src/components/MainContent.jsx
@@ -1,24 +1,23 @@
-import React from "react";
-import ProjectForm from "./ProjectForm";
-import ProjectDetails from "./ProjectDetails";
+import ProjectForm from './ProjectForm';
+import ProjectDetails from './ProjectDetails';
 
-const MainContent = ({ selectedProject, isAddedNewProject, ...props} ) => {
-  const { projects } = props
-  const projectCount = projects.length
-  
-  const showAddFirstProjectMessage = projectCount === 0 && !isAddedNewProject;
-  const showSelectProjectMessage = projectCount > 0 && !selectedProject && !isAddedNewProject;
-  const showProjectForm = isAddedNewProject;
-  const showProjectDetails = projectCount > 0 && selectedProject;
+const MainContent = ({ selectedProject, isAddedNewProject, ...props }) => {
+    const { projects } = props;
+    const projectCount = projects.length;
 
-  return (
-    <>
-      { showAddFirstProjectMessage  && <div>Add first project!</div>}
-      { showSelectProjectMessage  && <div>Select project!</div>}
-      { showProjectForm && <ProjectForm {...props} />}
-      { showProjectDetails && <ProjectDetails selectedProject={selectedProject} {...props} />}
-    </>
-  );
+    const showAddFirstProjectMessage = projectCount === 0 && !isAddedNewProject;
+    const showSelectProjectMessage = projectCount > 0 && !selectedProject && !isAddedNewProject;
+    const showProjectForm = isAddedNewProject;
+    const showProjectDetails = projectCount > 0 && selectedProject;
+
+    return (
+        <>
+            {showAddFirstProjectMessage && <div>Add first project!</div>}
+            {showSelectProjectMessage && <div>Select project!</div>}
+            {showProjectForm && <ProjectForm {...props} />}
+            {showProjectDetails && <ProjectDetails selectedProject={selectedProject} {...props} />}
+        </>
+    );
 };
 
 export default MainContent;
--- 02-starting-project/src/components/ProjectForm.jsx
+++ 02-starting-project/src/components/ProjectForm.jsx
@@ -1,27 +1,29 @@
-import React from 'react'
+const ProjectForm = ({ handleClickSaveProject, handleClickCancelProject, handleChangeInputsProject }) => {
+    return (
+        <form onSubmit={handleClickSaveProject} className="w-[50%]">
+            <h2 className="text-xl font-bold text-stone-500 my-4">Add Project</h2>
+            <div className="flex flex-row gap-4 justify-end">
+                <button onClick={(e) => handleClickCancelProject(e)} className="text-stone-700 hover:text-stone-950">
+                    Cancel
+                </button>
+                <button type="submit" className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">
+                    Save
+                </button>
+            </div>
+            <div className="flex flex-col gap-4">
+                <label className="text-sm font-bold uppercase text-stone-500">TITLE</label>
+                <input onChange={(e) => handleChangeInputsProject(e)} id="title" type="text" required className="w-64 px-2 py-1 rounded-sm bg-stone-200" />
+            </div>
+            <div className="flex flex-col gap-4">
+                <label className="text-sm font-bold uppercase text-stone-500">DESCRIPTION</label>
+                <input onChange={(e) => handleChangeInputsProject(e)} id="description" type="text" required className="w-64 px-2 py-1 rounded-sm bg-stone-200" />
+            </div>
+            <div className="flex flex-col gap-4">
+                <label className="text-sm font-bold uppercase text-stone-500">DUE DATE</label>
+                <input onChange={(e) => handleChangeInputsProject(e)} id="date" type="date" required className="w-64 px-2 py-1 rounded-sm bg-stone-200" />
+            </div>
+        </form>
+    );
+};
 
-const ProjectForm = ( { handleClickSaveProject, handleClickCancelProject, handleChangeInputsProject}) => {
-  return (
-     <form onSubmit={handleClickSaveProject} className="w-[50%]"> 
-          <h2 className="text-xl font-bold text-stone-500 my-4">Add Project</h2>
-          <div className="flex flex-row gap-4 justify-end">
-              <button onClick={(e) => handleClickCancelProject(e)} className="text-stone-700 hover:text-stone-950">Cancel</button>
-              <button type='submit' className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button>
-          </div>
-          <div className="flex flex-col gap-4">
-              <label className="text-sm font-bold uppercase text-stone-500">TITLE</label>
-              <input onChange={(e) => handleChangeInputsProject(e)} id="title" type="text" required className="w-64 px-2 py-1 rounded-sm bg-stone-200" />
-          </div>
-          <div className="flex flex-col gap-4">
-              <label className="text-sm font-bold uppercase text-stone-500">DESCRIPTION</label>
-              <input onChange={(e) => handleChangeInputsProject(e)} id="description" type="text" required  className="w-64 px-2 py-1 rounded-sm bg-stone-200" />
-          </div>
-          <div className="flex flex-col gap-4">
-              <label className="text-sm font-bold uppercase text-stone-500">DUE DATE</label>
-              <input onChange={(e) => handleChangeInputsProject(e)} id="date" type="date" required className="w-64 px-2 py-1 rounded-sm bg-stone-200" />
-          </div>
-      </form>
-  )
-}
-
-export default ProjectForm
+export default ProjectForm;
